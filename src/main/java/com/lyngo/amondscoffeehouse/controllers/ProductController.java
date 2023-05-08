6cbd3edc3f019bd714eacfeaf6e22fd53fa0c42c
lyngo-ntkl <lyngo.ntkl@gmail.com>
2023-05-08 10:05:08 +0700

    Add Swagger UI

--- src/main/java/com/lyngo/amondscoffeehouse/controllers/ProductController.java
+++ src/main/java/com/lyngo/amondscoffeehouse/controllers/ProductController.java
@@ -2,11 +2,10 @@ package com.lyngo.amondscoffeehouse.controllers;
 
 import com.lyngo.amondscoffeehouse.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "api/v1/products")
+@RequestMapping(path = "/api/v1/products")
 public class ProductController {
     @Autowired
     private ProductService productService;
